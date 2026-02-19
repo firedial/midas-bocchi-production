0007ca3ed3af7c293fa277a569e87387653bb619
firedial <42674772+firedial@users.noreply.github.com>
2026-02-19 20:52:57 +0900

    修正

--- ssh_setting.sh
+++ ssh_setting.sh
@@ -4,6 +4,12 @@
 read -p "ssh public key: " publicKey
 echo "$publicKey" >> ~/.ssh/authorized_keys
 
+echo "cat authorized_keys"
+echo "--------------------"
+cat ~/.ssh/authorized_keys
+echo "--------------------"
+read tmp
+
 # ssh allow from
 read -p "ssh allow from: " ipAddress
 
@@ -11,14 +17,32 @@ read -p "ssh allow from: " ipAddress
 echo "ALL: 127.0.0.1" >> /etc/hosts.allow
 echo "sshd: $ipAddress" >> /etc/hosts.allow
 
+echo "cat allow"
+echo "--------------------"
+cat /etc/hosts.allow
+echo "--------------------"
+read tmp
+
 # deny
 echo "ALL:ALL" >> /etc/hosts.deny
+
+echo "cat deny"
+echo "--------------------"
+cat /etc/hosts.deny
+echo "--------------------"
+read tmp
 
 # deny password login
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 
 # deny root login
-sed '/PermitRootLogin/d' /etc/ssh/sshd_config
+sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+
+echo "cat config"
+echo "--------------------"
+cat /etc/ssh/sshd_config
+echo "--------------------"
+read tmp
 
 systemctl restart sshd
