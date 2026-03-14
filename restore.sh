a70c5771e550f3c7de3f4b1a334505e444e5777e
firedial <42674772+firedial@users.noreply.github.com>
2026-03-15 01:29:34 +0900

    場所間違えていた

--- restore.sh
+++ restore.sh
@@ -27,8 +27,8 @@ while IFS='=' read -r key value; do
 done < .env
 
 echo "set PS1"
-echo "PS1='\[\e[${TERMINAL_ENV_COLOR}\][${APP_ENV}]\[\e[0m\] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] '" >> .bashrc
-source .bashrc
+echo "PS1='\[\e[${TERMINAL_ENV_COLOR}\][${APP_ENV}]\[\e[0m\] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] '" >> ~/.bashrc
+source ~/.bashrc
 
 echo "restore ssl"
 mkdir ssl
