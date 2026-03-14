5c6cf657a552bf17bfb55250b4bdb845accd613c
firedial <42674772+firedial@users.noreply.github.com>
2026-03-14 13:54:23 +0900

    yui のなかで自動実行するようにした

--- restore.sh
+++ restore.sh
@@ -33,21 +33,10 @@ echo ${SSL_CERT} | base64 -d > ssl/server.crt
 
 echo "image pull"
 docker compose pull
-echo "up rikka and yui"
-docker compose up -d rikka yui
-
-echo "sleep 30s"
-sleep 30
-
-echo "init query exec"
-docker compose exec yui /docker-entrypoint-initdb.d/001_create_midas_uesr.sh
-docker compose exec yui /docker-entrypoint-initdb.d/002_create_metabase.sh
-docker compose exec yui /docker-entrypoint-initdb.d/003_create_backup_user.sh
+echo "up all"
+docker compose up -d --wait
 
 echo "midas restore"
 docker compose exec rikka /app/restore/crypt_restore.sh
 echo "metabase restore"
 docker compose exec rikka /app/restore/crypt_metabase_restore.sh
-
-echo "up all"
-docker compose up -d
