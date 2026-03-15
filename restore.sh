8c2be8cebecd6cf94c8cdd856554bb82700773ae
firedial <42674772+firedial@users.noreply.github.com>
2026-03-15 12:46:18 +0900

    起動時に実行できるようになったので削除

--- restore.sh
+++ restore.sh
@@ -42,11 +42,6 @@ docker compose up -d rikka yui
 echo "sleep 30s"
 sleep 30
 
-echo "init query exec"
-docker compose exec yui /docker-entrypoint-initdb.d/001_create_midas_uesr.sh
-docker compose exec yui /docker-entrypoint-initdb.d/002_create_metabase.sh
-docker compose exec yui /docker-entrypoint-initdb.d/003_create_backup_user.sh
-
 echo "midas restore"
 docker compose exec rikka /app/restore/crypt_restore.sh
 echo "metabase restore"
